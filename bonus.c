9a9adfb080e546ec838a81e4de767095303828af
Minguk Gaang <minguk.gaang@gmail.com>
2023-04-10 13:16:28 +0200

    ft_list_push_front good

--- bonus.c
+++ bonus.c
@@ -1,14 +1,31 @@
 #include "libasm.h"
 
+void print_list(t_list *list) {
+    while (list) {
+        printf("loop...!\n");
+        printf("%s\n", list->data);
+        list = list->next;
+    }
+}
+
 int main() {
     extern int  ft_atoi_base(char *str, char *base);
-    extern int  check_base(char *base);
     extern void ft_list_push_front(t_list **begin_list, void *data);
     extern int  ft_list_size(t_list *begin_list);
     extern void ft_list_sort(t_list **begin_list, int (*cmp)());
     extern void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *));
 
-    char *base = "0123456789abcdef";
+    //char *base = "0123456789abcdef";
+    //printf("test %i\n", ft_atoi_base("  +--+-a", base));
 
-    printf("test %i\n", ft_atoi_base("  +--+-a", base));
+    t_list *test = malloc(sizeof(t_list));
+    test->data = "test";
+    test->next = NULL;
+    ft_list_push_front(&test, "test2");
+    print_list(test);
+
+    t_list *test2 = NULL;
+    ft_list_push_front(&test2, "test3");
+    print_list(test2);
+
 }
--- src/ft_list_push_front.s
+++ src/ft_list_push_front.s
@@ -2,19 +2,25 @@ global _ft_list_push_front
 extern _malloc
 extern ___error
 
+;typedef struct s_list {
+;	void			*data;
+;	struct s_list	*next;
+;} t_list;
+
 ; void ft_list_push_front(t_list **begin_list, void *data)
 
 _ft_list_push_front:
     enter 8, 0
     mov r12, [rdi]
+    mov r13, rsi
     push rdi
     mov rdi, 0x10
     call _malloc
     cmp rax, 0
     je err
-    mov [rax], rsi
-    mov [rax + 8], r12
     pop rdi
+    mov [rax], r13
+    mov [rax + 8], r12
     mov [rdi], rax
 end:
     leave
